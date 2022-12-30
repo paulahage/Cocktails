1cce3708d6fc00119c33bfd7c6a9ec1b68752c94
paula hage <hage.paula@hotmail.com>
2022-12-30 17:05:49 +0100

    Cocktails project

---
+++ website/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus,
+        deleniti. Mollitia aspernatur, veniam dolorem dicta, earum accusamus quo
+        vel cumque aperiam minus eum nam eius reprehenderit facilis, nostrum
+        provident fuga.
+      </p>
+    </section>
+  );
+};
+
+export default About;
